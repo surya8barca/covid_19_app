8e143b96a344ecb3739b88bb98dd641db781c58d
surya8barca <surya8barca@gmail.com>
2020-04-22 12:26:36 +0530

    start

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Home()
+)
+);
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+        title: Text(
+          'Covid-19',
+          style: TextStyle(
+            fontSize: 28.0,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
