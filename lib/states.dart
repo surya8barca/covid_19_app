7ee6860d4ab13dd0f30161d29f0ee8b917f0a814
surya8barca <surya8barca@gmail.com>
2020-04-23 13:37:59 +0530

    drop down list - states

--- lib/states.dart
+++ lib/states.dart
@@ -2,6 +2,7 @@
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart';
 
 void main() => runApp(MaterialApp(
@@ -17,7 +18,8 @@ class States extends StatefulWidget {
 class _HomeState extends State<States> {
   
   Map info;
-  List states;
+  List<String> states;
+  String stateSelected;
 
   Future<void> stateData() async {
     final data = await get('https://covid19indiaapi.herokuapp.com/v1/states');
@@ -25,7 +27,7 @@ class _HomeState extends State<States> {
     setState(() {
       states=info.keys.toList();
     });
-    print(states);    
+    print(states);   
   }
 
   @override
@@ -35,7 +37,18 @@ class _HomeState extends State<States> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  if ((info == null)) {
+      return Scaffold(
+            backgroundColor: Colors.lightBlue,
+            body: Center(
+              child: SpinKitHourGlass(
+                color: Colors.red,
+                size: 200.0,
+              ),
+            ),
+          );
+    } else {
+      return Scaffold(
       backgroundColor: Colors.lightBlue,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
@@ -50,9 +63,41 @@ class _HomeState extends State<States> {
       ),
       body: SingleChildScrollView(
         child:Container(
-          child: Center(),
+          padding: EdgeInsets.all(10),
+          child: DropdownButton(
+            underline: Container(),
+            icon: Icon(Icons.arrow_drop_down_circle),
+            iconEnabledColor: Colors.black,
+            iconSize: 50,
+            isDense: true,
+            hint: Text(
+              'Select State ->',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+              ),
+            ),
+            items: states.map<DropdownMenuItem<String>>((value){
+              return DropdownMenuItem<String>(
+                child: Text(value),
+                value: value,
+              );
+            }).toList(),
+            style: TextStyle(
+              color:Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+            onChanged: (chosen){
+              setState(() {
+                stateSelected=chosen;   
+              });
+              print(stateSelected);
+            },
+            value: stateSelected,
+          ),
         )
       ),
     );
+    }
   }
 }
